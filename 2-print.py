7d6bdef695c55f70178f861524190f0758f94453
RenatMirzayev <renadmirzoyev@gmail.com>
2025-01-07 18:58:30 +0400

    pythonnn

--- 2-print.py
+++ 2-print.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-print("Programming is like building a multilingual puzzle\n")
-        i/ Only print the prompt if stdin is a terminal (interactive mode)
+print("Programming is like building a multilingual puzzle")
